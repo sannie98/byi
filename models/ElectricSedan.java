eb5fceb55b5f9a8618ce3fd739ba7585316d6c4e
sannie98 <leeminso76@gmail.com>
2025-10-08 14:26:49 +0300

    Delete models directory

--- models/ElectricSedan.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class ElectricSedan extends ElectroCar {
-    public ElectricSedan(int year, int horsePower, String brand, int batteryCapacity) {
-        super(year, horsePower, brand, batteryCapacity);
-    }
-}
